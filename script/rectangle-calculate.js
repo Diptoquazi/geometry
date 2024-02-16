f7894a4f4af973b6505ce2ce1a9990103d364af8
Quazi A.Q.M. Ziaus Shams <155340045+Diptoquazi@users.noreply.github.com>
2024-02-16 19:02:58 +0600

    added rectamgle

--- script/rectangle-calculate.js
+++ script/rectangle-calculate.js
@@ -1,3 +1,17 @@
 function calculateRectangleArea(){
-    console.log('calculate rect area');
+   const lenghtInput = document.getElementById('rectangle-base');
+   const lenghtText = lenghtInput.value;
+   const length = parseFloat(lenghtText);
+   console.log(length);
+
+   const widthInput = document.getElementById('rectangle-height');
+   const widthText = widthInput.value;
+   const width = parseFloat(widthText);
+   console.log(width);
+
+   const area = length * width;
+   console.log(area);
+
+   const rectangleAreaSpan = document.getElementById('rectangle-area');
+   rectangleAreaSpan.innerText = area;
 }
